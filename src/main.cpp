26a6038871a604199bf7c9e63dff8e674e5c754e
Proyecto Final <fran.cariglino06@gmail.com>
2024-10-25 08:34:57 -0300

    Add .gitattributes to enforce LF for .vscode/extensions .json

---
+++ src/main.cpp
@@ -0,0 +1,337 @@
+/*int NUMBER9[] = {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0};
+int NUMBER8[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1};
+int NUMBER7[] = {1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1};
+int NUMBER6[] = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+int NUMBER5[] = {1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int NUMBER4[] = {1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1};
+int NUMBER3[] = {0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int NUMBER2[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+int _[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int A[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+int B[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0};
+int C[] = {0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1};
+int D[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0};
+int E[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1};
+int F[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0};
+int G[] = {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0};
+int H[] = {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+int I[] = {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1};
+int J[] = {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0};
+int K[] = {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1};
+int L[] = {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1};
+int M[] = {1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+int N[] = {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+int O[] = {0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0};
+int P[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0};
+int Q[] = {0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+int R[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1};
+int S[] = {0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0};
+int T[] = {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+int U[] = {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0};
+int V[] = {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0};
+int W[] = {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+int X[] = {1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1};
+int Y[] = {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0};
+int Z[] = {1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1};
+int* alpha[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N}; //,T,U,V,W,X,Y,Z};
+int letterSpace;
+int dotTime;
+void setup()
+{ Serial.begin(9600);
+  // setting the ports of the leds to OUTPUT(SExperiments)
+  for ( int i = 2; i < 10 ; i++ )
+  { pinMode(i, OUTPUT);
+  }
+
+  // defining the space between the letters (ms)
+  letterSpace = 6;
+  // defining the time dots appear (ms)
+  dotTime = 1;
+}
+void printLetter(int letter[])
+{
+  int y;
+  // printing the first y row of the letter
+  for (y = 0; y < 8; y++)
+  {
+    digitalWrite(y + 2, letter[y]);
+  }
+  delay(dotTime);
+  // printing the second y row of the letter
+  for (y = 0; y < 8; y++)
+  {
+    digitalWrite(y + 2, letter[y + 8]);
+  }
+  delay(dotTime);
+  // printing the third y row of the letter
+  //(SExperiments)
+  for (y = 0; y < 8; y++)
+  {
+    digitalWrite(y + 2, letter[y + 16]);
+  }
+  delay(dotTime);
+  for (y = 0; y < 8; y++) {
+    digitalWrite(y + 2, letter[y + 24]);
+  }
+  delay(dotTime);
+  for (y = 0; y < 8; y++) {
+    digitalWrite(y + 2, letter[y + 32]);
+  }
+  delay(dotTime);
+  // printing the sspace between the letters
+  //(SExperiments)
+  for (y = 0; y < 8; y++)
+  {
+    digitalWrite(y + 2, 0);
+  }
+  delay(letterSpace);
+}
+void loop()//write here =)
+{
+  printLetter (W);
+  printLetter (E);
+  printLetter (L);
+  printLetter (C);
+  printLetter (O);
+  printLetter (M);
+  printLetter (E);
+  printLetter (_); //Space
+  printLetter (_); //Space
+}
+*/
+
+#include <Adafruit_NeoPixel.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
+#define PINLEDS 15
+#define LEDS 16
+
+WebServer server(80);
+
+// Configura tu red Wi-Fi
+const char* ssid = "Aceituna";
+const char* password = "gordobarassi123";
+
+Adafruit_NeoPixel pixels(LEDS, PINLEDS, NEO_GRB + NEO_KHZ800);
+
+typedef enum { letraA1, letraA2, letraA3} LetraA;
+typedef enum { letraB1, letraB2, letraB3} LetraB;
+typedef enum { letraC1, letraC2,} LetraC;
+typedef enum { letraD1, letraD2} LetraD;
+typedef enum { letraE1, letraE2,letraE3} LetraE;
+typedef enum { letraF1, letraF2} LetraF;
+typedef enum { letraG1, letraG2, letraG3} LetraG;
+typedef enum { letraH1, letraH2} LetraH;
+typedef enum { letraI1, letraI2, letraI3} LetraI;
+typedef enum { letraJ1, letraJ2, letraJ3} LetraJ;
+typedef enum { letraK1, letraK2, letraK3} LetraK;
+typedef enum { letraL1, letraL2} LetraL;
+typedef enum { letraM1, letraM2, letraM3, letraM4} LetraM;
+typedef enum { letraN1, letraN2, letraN3, letraN4, letraN5} LetraN;
+typedef enum { letra_N1, letra_N2, letra_N3, letra_N4, letra_N5} Letra_N;
+typedef enum { letraO1, letraO2} LetraO;
+typedef enum { letraP1, letraP2, letraP3} LetraP;   
+typedef enum { letraQ1, letraQ2, letraQ3} LetraQ;
+typedef enum { letraR1, letraR2, letraR3} LetraR;
+typedef enum { letraS1, letraS2, letraS3} LetraS;
+typedef enum { letraT1, letraT2} LetraT;
+typedef enum { letraU1, letraU2} LetraU;
+typedef enum { letraV1, letraV2, letraV3} LetraV;
+typedef enum { letraW1, letraW2,letraW3, letraW4, letraW5} LetraW;
+typedef enum { letraX1, letraX2} LetraX;
+typedef enum { letraY1, letraY2, letraY3} LetraY;
+typedef enum { letraZ1, letraZ2, letraZ3, letraZ4, letraZ5} LetraZ;
+
+LetraA letra_a;
+LetraB letra_b;
+LetraC letra_c;
+LetraD letra_d;
+LetraE letra_e;
+LetraF letra_f;
+LetraG letra_g;
+LetraH letra_h;
+LetraI letra_i;
+LetraJ letra_j;
+LetraK letra_k;
+LetraL letra_l;
+LetraM letra_m;
+LetraN letra_n;
+Letra_N letra__n;
+LetraO letra_o;
+LetraP letra_p;
+LetraQ letra_q;
+LetraR letra_r;
+LetraS letra_s;
+LetraT letra_t;
+LetraU letra_u;
+LetraV letra_v;
+LetraW letra_w;
+LetraX letra_x;
+LetraY letra_y;
+LetraZ letra_z;
+
+void setup() {
+  Serial.begin(115200);
+  pixels.begin();
+  pixels.clear();
+  pixels.show();
+
+  letra_a = letraA1;
+  letra_b = letraB1;
+  letra_c = letraC1;
+  letra_d = letraD1;
+  letra_e = letraE1;
+  letra_f = letraF1;
+  letra_g = letraG1;
+  letra_h = letraH1;
+  letra_i = letraI1;
+  letra_j = letraJ1;
+  letra_k = letraK1;
+  letra_l = letraL1;
+  letra_m = letraM1;
+  letra_n = letraN1;
+  letra__n = letra_N1;
+  letra_o = letraO1;
+  letra_p = letraP1;
+  letra_q = letraQ1;
+  letra_r = letraR1;
+  letra_s = letraS1;
+  letra_t = letraT1;
+  letra_u = letraU1;
+  letra_v = letraV1;
+  letra_w = letraW1;
+  letra_x = letraX1;
+  letra_y = letraY1;
+  letra_z = letraZ1;
+
+  // Conectar a Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Conectado a WiFi");
+  Serial.print("IP del ESP32: ");
+  Serial.println(WiFi.localIP());
+
+  // Configurar el servidor web
+  server.on("/", pagina);  // Página principal
+  server.on("/update", gestionSolicitudes);  // Ruta para recibir datos
+  server.begin();
+  Serial.println("Servidor web iniciado");
+}
+
+void loop() {
+  server.handleClient();  // Escucha solicitudes del cliente web
+}
+
+void mostrarLetra(char letra, int letra_tipo) {
+// Lógica para mostrar la letra A
+if (letra == 'A') {
+	switch (letra_tipo) {
+		case letraA1:
+			pixels.setPixelColor(0, 0, 0, 0);
+			pixels.setPixelColor(1, 0, 0, 0);
+			pixels.setPixelColor(2, 255, 0, 0);
+			pixels.setPixelColor(3, 255, 0, 0);
+			pixels.setPixelColor(4, 255, 0, 0);
+			pixels.setPixelColor(5, 255, 0, 0);
+			pixels.setPixelColor(6, 255, 0, 0);
+			pixels.setPixelColor(7, 0, 0, 0);
+			break;
+		case letraA2:
+			pixels.setPixelColor(0, 0, 0, 0);
+			pixels.setPixelColor(1, 0, 0, 0);
+			pixels.setPixelColor(2, 255, 0, 0);
+			pixels.setPixelColor(3, 0, 0, 0);
+			pixels.setPixelColor(4, 255, 0, 0);
+			pixels.setPixelColor(5, 0, 0, 0);
+			pixels.setPixelColor(6, 0, 0, 0);
+			pixels.setPixelColor(7, 0, 0, 0);
+			break;
+		case letraA3:
+			pixels.setPixelColor(0, 0, 0, 0);
+			pixels.setPixelColor(1, 0, 0, 0);
+			pixels.setPixelColor(2, 255, 0, 0);
+			pixels.setPixelColor(3, 255, 0, 0);
+			pixels.setPixelColor(4, 255, 0, 0);
+			pixels.setPixelColor(5, 255, 0, 0);
+			pixels.setPixelColor(6, 255, 0, 0);
+			pixels.setPixelColor(7, 0, 0, 0);
+			break;
+		}
+}
+
+// Lógica para mostrar la letra B
+else if (letra == 'B') {
+    switch (letra_tipo) {
+        case letraB1:
+            pixels.setPixelColor(0, 0, 0, 0);
+            pixels.setPixelColor(1, 0, 0, 0);
+            pixels.setPixelColor(2, 255, 0, 0);
+            pixels.setPixelColor(3, 255, 0, 0);
+            pixels.setPixelColor(4, 255, 0, 0);
+            pixels.setPixelColor(5, 255, 0, 0);
+            pixels.setPixelColor(6, 255, 0, 0);
+            pixels.setPixelColor(7, 0, 0, 0);
+            break;
+        case letraB2:
+            pixels.setPixelColor(0, 0, 0, 0);
+            pixels.setPixelColor(1, 0, 0, 0);
+            pixels.setPixelColor(2, 255, 0, 0);
+            pixels.setPixelColor(3, 0, 0, 0);
+            pixels.setPixelColor(4, 255, 0, 0);
+            pixels.setPixelColor(5, 0, 0, 0);
+            pixels.setPixelColor(6, 255, 0, 0);
+            pixels.setPixelColor(7, 0, 0, 0);
+            break;
+        case letraB3:
+            pixels.setPixelColor(0, 0, 0, 0);
+            pixels.setPixelColor(1, 0, 0, 0);
+            pixels.setPixelColor(2, 0, 0, 0);
+            pixels.setPixelColor(3, 255, 0, 0);
+            pixels.setPixelColor(4, 0, 0, 0);
+            pixels.setPixelColor(5, 255, 0, 0);
+            pixels.setPixelColor(6, 0, 0, 0);
+            pixels.setPixelColor(7, 0, 0, 0);
+            break;
+    }
+}
+
+// Lógica para mostrar la letra C
+else if (letra == 'C') {
+    switch (letra_tipo) {
+        case letraC1:
+            pixels.setPixelColor(0, 0, 0, 0);
+            pixels.setPixelColor(1, 0, 0, 0);
+            pixels.setPixelColor(2, 255, 0, 0);
+            pixels.setPixelColor(3, 255, 0, 0);
+            pixels.setPixelColor(4, 255, 0, 0);
+            pixels.setPixelColor(5, 255, 0, 0);
+            pixels.setPixelColor(6, 255, 0, 0);
+            pixels.setPixelColor(7, 0, 0, 0);
+            break;
+        case letraC2:
+            pixels.setPixelColor(0, 0, 0, 0);
+            pixels.setPixelColor(1, 0, 0, 0);
+            pixels.setPixelColor(2, 255, 0, 0);
+            pixels.setPixelColor(3, 0, 0, 0);
+            pixels.setPixelColor(4, 0, 0, 0);
+            pixels.setPixelColor(5, 0, 0, 0);
+            pixels.setPixelColor(6, 255, 0, 0);
+            pixels.setPixelColor(7, 0, 0, 0);
+            break;
+    }
+}
+
+pixels.show();
+}
+void pagina() {
+  String page = 
+}
+
+void gestionSolicitudes() {
+
+}
